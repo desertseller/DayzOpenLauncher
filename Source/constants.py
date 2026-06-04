0a9ba88541c809da2c11646624b52f600bb8f316
Dese193 <pawelk193@hotmail.com>
2026-06-04 20:45:23 +0200

    ver

--- Source/constants.py
+++ Source/constants.py
@@ -1,5 +1,5 @@
 VERSION = "1.2.7"
-BUILD_INFO = "040620263"
+BUILD_INFO = "040620261"
 APP_NAME = "DayzOpenLauncher"
 DZSA_API_URL = "https://dayzsalauncher.com/api/v2/launcher/servers/dayz"
 CONFIG_FILE_NAME = "config.json"
